08f81270e38ba333e2f5121a4511ccd52d668f05
mi3er <mi3er@bk.ru>
2023-07-24 20:40:49 +0300

    Change name in textField

--- m16Homework/ViewController.swift
+++ m16Homework/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textLabel.text = "Pavel"
+        textLabel.text = "Pavel_F"
     }
 }
 
