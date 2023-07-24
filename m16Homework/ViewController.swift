35d2265bc09530beb0ccdf24abf7c0fbea606318
mi3er <mi3er@bk.ru>
2023-07-24 20:32:53 +0300

    Change name in text Label

--- m16Homework/ViewController.swift
+++ m16Homework/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textLabel.text = "Student name"
+        textLabel.text = "Pavel"
     }
 }
 
